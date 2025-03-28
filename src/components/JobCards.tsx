0bc14d70eaba27ef0b3211d062e64a6857232888
imspdr <konglsh@naver.com>
2025-03-28 17:40:04 +0900

    card height

--- src/components/JobCards.tsx
+++ src/components/JobCards.tsx
@@ -9,7 +9,7 @@ const jobCardContainer = (width: number, padding: number, hover: boolean) => css
   justify-content: space-between;
   border-radius: 10px;
   width: ${width - padding * 2}px;
-  height: ${width / 4 - padding * 2}px;
+  height: ${width / 3.8 - padding * 2}px;
   padding: ${padding + (hover ? -1 : 0)}px;
   background-color: var(--paper);
   overflow: hidden;
